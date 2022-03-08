3de264812ce765b62543f544e49a2c4ec48779a3
DavidEspinozaNemo <david1998es16@gmail.com>
2022-03-08 14:56:27 -0600

    Update datosPrueba.sql

--- SQL/datosPrueba.sql
+++ SQL/datosPrueba.sql
@@ -5,3 +5,20 @@ INSERT INTO CANTON (canton_name, province_id) VALUES ('Marjayoûn', 7),('Jingkou
 INSERT INTO USER_TYPE (user_type_name) VALUES ('Admin de Campaña'), ('Ciudadano')
 INSERT INTO KPI_TYPE (kpi_name) VALUES ('eu'), ('nulla'), ('odio'), ('rhoncus'), ('cras'), ('eget'), ('eutsata'), ('purus'), ('nunc'), ('luctus'), ('ipsum'), ('suspendisse'), ('molestie'), ('nulltsartara'), ('pede'), ('tellus'), ('vestibulum'), ('habitasse'), ('mi'), ('orci')
 INSERT INTO PARTY (party_name, flag, creation_date, foundation) VALUES ('Russel, Rippin and Schmidt', 'http://dummyimage.com/178x100.png/cc0000/ffffff', '10/13/1995', '7/7/2019'), ('Mitchell, Heller and Kirlin', 'http://dummyimage.com/195x100.png/cc0000/ffffff', '6/30/2014', '12/7/2002'), ('Stoltenberg-Crooks', 'http://dummyimage.com/249x100.png/cc0000/ffffff', '6/2/2013', '6/20/1998'), ('Huel-Leannon', 'http://dummyimage.com/153x100.png/ff4444/ffffff', '9/15/2010', '2/22/2017'), ('Kulas-Sanford', 'http://dummyimage.com/204x100.png/cc0000/ffffff', '6/21/2001', '5/16/2006')
+
+EXEC sp_create_person 'Daniel', 'Soto Valverde', '1/16/2014', 123456790, 'Fāqūs', 'danielsoto@gmail.com', 25500001, 'Ciudadano';
+EXEC sp_create_person 'Jill', 'Webb', '1/17/2014', 123456791, 'Fāqūs', 'jill@gmail.com', 25500002, 'Ciudadano';
+EXEC sp_create_person 'Jessica', 'Armstrong', '1/18/2014', 123456792, 'Fāqūs', 'jessica@gmail.com', 25500003, 'Ciudadano';
+EXEC sp_create_person 'Henry', 'Villa', '1/19/2015', 123456793, 'Fāqūs', 'henry@gmail.com', 25500004, 'Ciudadano';
+EXEC sp_create_person 'Jeffrey', 'Campbell', '1/20/2004', 123456794, 'Fāqūs', 'jeffrey@gmail.com', 25500005, 'Ciudadano';
+EXEC sp_create_person 'Stephanie', 'Lewis', '2/1/2004', 123456795, 'Fāqūs', 'stephanie@gmail.com', 25500006, 'Ciudadano';
+EXEC sp_create_person 'Teresa', 'Ray', '2/2/2014', 123456796, 'Fāqūs', 'teresa@gmail.com', 25500007, 'Ciudadano';
+EXEC sp_create_person 'Shannon', 'Bean', '2/3/2014', 123456797, 'Fāqūs', 'shannon@gmail.com', 25500008, 'Ciudadano';
+EXEC sp_create_person 'Rachel', 'Yates', '2/4/2004', 123456798, 'Fāqūs', 'rachel@gmail.com', 25500009, 'Ciudadano';
+EXEC sp_create_person 'Kevin', 'Lewis', '2/5/2014', 123456799, 'Fāqūs', 'kevin@gmail.com', 25500010, 'Ciudadano';
+EXEC sp_create_person 'Kenneth', 'Chandler', '1/16/2010', 123456801, 'Fāqūs', 'kenneth@gmail.com', 25500011, 'Ciudadano';
+EXEC sp_create_person 'Bryan', 'Moreno', '1/17/2014', 123456802, 'Fāqūs', 'bryan@gmail.com', 25500012, 'Ciudadano';
+EXEC sp_create_person 'Kelly', 'Zhang', '1/18/2003', 123456800, 'Fāqūs', 'kelly@gmail.com', 25500013, 'Ciudadano';
+EXEC sp_create_person 'Shane', 'Gardner', '1/19/2015', 123456803, 'Fāqūs', 'shane@gmail.com', 25500014, 'Ciudadano';
+EXEC sp_create_person 'Matthew', 'Harris', '1/20/2014', 123456804, 'Fāqūs', 'matthew@gmail.com', 25500016, 'Ciudadano';
+EXEC sp_create_person 'Janice', 'Moore', '1/16/2010', 123456805, 'Fāqūs', 'janice@gmail.com', 25500017, 'Ciudadano';
