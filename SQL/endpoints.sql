0dfefbdc43610031373f2dc1d3a9ec3351b917fe
DavidEspinozaNemo <david1998es16@gmail.com>
2022-03-13 02:40:33 -0600

    endpoint01

--- SQL/endpoints.sql
+++ SQL/endpoints.sql
@@ -1,6 +1,43 @@
 USE Caso02_DB2_Esteban_David;
 GO
 -- Endpoint #01
+-- Para listar los cantones que recibiran entregables los primero 100 dias de gobierno,
+-- pero no recibiran nada los ultimos 100 dias
+-- salida: Partido x Canton x cantidad de beneficios
+CREATE PROCEDURE sp_endpoint01(
+	@party_id INT,
+	@canton_id INT
+)
+AS
+BEGIN
+	DECLARE @first_day date;
+	DECLARE @last_day date;
+
+	SELECT @first_day = MIN(post_time) FROM DELIVERABLES;
+	SELECT @last_day = MAX(post_time) FROM DELIVERABLES;
+
+	-- todo lo de la izquierda que no este en la derecha
+	SELECT p.party_name as 'Partido', c.canton_name as 'Canton' ,COUNT(d.delivery_id) as 'Cantidad de Beneficios'
+	FROM DELIVERABLES as d
+	INNER JOIN CANTON as c ON d.canton_id = c.canton_id
+	INNER JOIN CAMPAIGN_MANAGERS as cm ON d.action_id = cm.campain_manager_id
+	INNER JOIN PARTY as p ON cm.party_id = p.party_id
+	WHERE d.post_time BETWEEN @first_day AND DATEADD(day, 100, @first_day)
+	AND p.party_id = ISNULL(@party_id, p.party_id)
+	AND c.canton_id = ISNULL(@canton_id, c.canton_id)
+	GROUP BY p.party_name, c.canton_name
+	EXCEPT
+	SELECT p.party_name as 'Partido', c.canton_name as 'Canton' ,COUNT(d.delivery_id) as 'Cantidad de Beneficios'
+	FROM DELIVERABLES as d
+	INNER JOIN CANTON as c ON d.canton_id = c.canton_id
+	INNER JOIN CAMPAIGN_MANAGERS as cm ON d.action_id = cm.campain_manager_id
+	INNER JOIN PARTY as p ON cm.party_id = p.party_id
+	WHERE d.post_time BETWEEN DATEADD(day, -100, @last_day) AND @first_day
+	AND p.party_id = ISNULL(@party_id, p.party_id)
+	AND c.canton_id = ISNULL(@canton_id, c.canton_id)
+	GROUP BY p.party_name, c.canton_name;
+END
+;
 GO 
 -- Endpoint #02
 --Para una misma acción en un mismo partido, 
